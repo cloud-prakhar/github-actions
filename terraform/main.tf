7785b4aa386c62224b8f7d65e77f98a056b7ecba
Prakhar <cloud.prakhargupta@gmail.com>
2026-07-19 10:52:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,10 @@
 
+resource "terraform_data" "example" {
+  input = {
+    message = "This root module stores its state in the bootstrapped S3 backend."
+  }
+}
+
+output "example_message" {
+  value = terraform_data.example.output.message
+}
